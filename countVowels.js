416b322bb1fcd72d07416137c108b2111ad5b5a0
manisaparajuli <manisaparajuli66@gmail.com>
2024-06-02 20:48:01 +1000

    count vawels in the sentence

--- countVowels.js
+++ countVowels.js
@@ -1,5 +1,12 @@
-
 function vawels(str){
-  
+  const vowelsToCount = ['a', 'e', 'i', 'o', 'u'];
+  let currentCount = 0
+
+  for(let letter of str){
+    if (vowelsToCount.includes(letter) ){
+      currentCount += 1
+    }
+  }
+  return currentCount
 }
-console.log("manisha parajuli")
+console.log(vawels("manisha parajuli"))
