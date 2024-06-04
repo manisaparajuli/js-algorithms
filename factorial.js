18f4b8c7fa3d220ebe41649558474902c926e63c
manisaparajuli <manisaparajuli66@gmail.com>
2024-06-04 16:05:52 +1000

    factorial using for loop

---
+++ factorial.js
@@ -0,0 +1,8 @@
+function factorial(n){
+  let fac = 1   
+  for(let i = 1; i <= n; i++){
+    fac = fac * i
+  }
+  return fac
+}
+console.log(factorial(4))
---
+++ pyramidPattern.js
@@ -0,0 +1,6 @@
+function pyramidPattern(n){
+  for(let row=1; row<=n; row++){
+    
+  }
+}
+console.log(pyramidPattern(5))
