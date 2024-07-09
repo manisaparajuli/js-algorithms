623675caa97a97874079e0e4fd8039bc40c608a3
manisaparajuli <manisaparajuli66@gmail.com>
2024-07-09 13:56:52 +1000

    anagram

--- anagrams.js
+++ anagrams.js
@@ -1,15 +1,17 @@
-function charmap(str){
-  let charMap = {}
-  str = str.toLowerCase().replace(/[\W]/g,'')
-  for(let char of str){
-    charMap[char] = charMap[char] + 1 || 1
+function isAnagram(s, t){
+  if(s.length !== t.length) return false;
+  let sMap = {}
+  let tMap = {}
+  for(let i = 0; i<s.length; i++){
+    sMap[s[i]] = 1+ (sMap[s[i]] || 0)
+    tMap[t[i]] = 1+ (tMap[t[i]] || 0)
   }
-  return charMap
+  for (const key in sMap){
+    if(sMap[key] !== tMap[key]){
+      return false
+    }
+  }
+  return true;
 }
 
-function anagrams(stringA, stringB){
-  const carMapA = charmap(stringA)
-  const carMapB = charmap(stringB)
-
-}
-console.log(anagrams("hello Manisha", "manisha hello"))
+console.log(isAnagram("potato", "tpotao"))
